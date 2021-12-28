4fa9944cac8bd66789fd4049ca095523c6dd009a
Abhay420000 <abhaykumar420000@gmail.com>
2021-12-28 17:39:45 +0530

    Multiply Added

--- main.cpp
+++ main.cpp
@@ -10,8 +10,14 @@ int factorial(int num)
     return num*factorial(num-1);
 }
 
+int multiply(int n1, int n2)
+{
+    return n1*n2;
+}
+
 int main()
 {
     cout << factorial(10) << endl;
+    cout << multiply(10,10) << end;
     return 0;
 }
