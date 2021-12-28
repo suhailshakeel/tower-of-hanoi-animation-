ba0ebe337f3766e308aff6251ba1308f1de8fab3
suhailshakeel <salroosahil2875@gmail.com>
2021-12-28 17:38:50 +0530

    modified again

--- main.cpp
+++ main.cpp
@@ -9,9 +9,21 @@ int factorial(int num)
 
     return num*factorial(num-1);
 }
+void sum(int num)
+{
+    int i;
+    for(i=0;i<num;i++)
+    {
+        if(i %2 == 0)
+        {
+            cout<<i<<endl;
+        }
+    }
+}
 
 int main()
 {
     cout << factorial(10) << endl;
+    sum(10000);
     return 0;
 }
