0241b1212baa3ebaee998cc9a14be7d5d1a3bc30
Abhay420000 <abhaykumar420000@gmail.com>
2021-12-28 17:31:28 +0530

    Factorial Function Builded

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+using namespace std;
+
+int factorial(int num)
+{
+    if (num == 1)
+        return 1;
+
+    return num*factorial(num-1);
+}
+
+int main()
+{
+    cout << factorial(10) << endl;
+    return 0;
+}
